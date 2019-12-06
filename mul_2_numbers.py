20bff18633cce30e333ea4d052ead2559140c868
Karvendhan <karvsmech@gmail.com>
2019-12-07 04:20:05 +0530

    alteration: logic of functions number of digits and is even

--- mul_2_numbers.py
+++ mul_2_numbers.py
@@ -1,5 +1,7 @@
 # A recursive algorithm to multiply two numbers
 
+
+# A function must have two blank lines before it
 def count_number_of_digits(num):
     '''
 
@@ -7,14 +9,22 @@ def count_number_of_digits(num):
     :return: The number of digits in the integer
     '''
 
-    if (isinstance(num, int)) & (num > 0):
+    if (isinstance(num, int)) | (isinstance(num, float)):
+        num = abs(int(num))
+
+        # if the given number is zero itself, the it has 1 digit
+        if num == 0:
+            return 1
+
         count = 0
+
         while num > 0:
             num = num // 10
             count += 1
         return count
     else:
-        print(f'the given input {num} must be a positive integer')
+        print(f'the given input {num} must be a number')
+
 
 def is_even(num):
     '''
@@ -23,13 +33,12 @@ def is_even(num):
     :return: The number is even or not
     '''
     if (isinstance(num, int)) | (isinstance(num, float)):
-        if num%2==0:
+        if num % 2 == 0:
             return True
         else:
             return False
     else:
-        print(f'the given input {num} must be an integer or a float')
+        print(f'the given input {num} must be a number')
 
-x = int(input('please enter a number(integer)'))
-y = int(input('please enter another number(integer)'))
-
+# x = int(input('please enter a number(integer)'))
+# y = int(input('please enter another number(integer)'))
