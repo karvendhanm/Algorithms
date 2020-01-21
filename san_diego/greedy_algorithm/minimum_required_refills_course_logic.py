6281f8942b49ff97d8188250e50f9998ef02f8a0
Karvendhan <karvsmech@gmail.com>
2020-01-22 04:50:03 +0530

    car refueling course logic comments

--- san_diego/greedy_algorithm/minimum_required_refills_course_logic.py
+++ san_diego/greedy_algorithm/minimum_required_refills_course_logic.py
@@ -1,19 +1,23 @@
+# We need to go from point A to point B.
+# There are n gas stations in between A and B.
+# x - array of distance of gas stations from point A. note: both starting point(A, 0kms) and the destination(B) have also been included in the array x.
+# n - number of gas stations in between A and B
+# L - number of kilometers that can be travelled in single refueling.
+
 def minimum_refills(x, n, L):
-    num_refills = 0
-    current_refill = 0
+    num_refills, current_refill = 0, 0
 
     while current_refill <= n:
         last_refill = current_refill
         while (current_refill <= n) and ((x[current_refill+1] - x[last_refill]) <= L):
             current_refill += 1
-        if current_refill == last_refill:
+        if last_refill == current_refill:
             return "IMPOSSIBLE"
         if current_refill <= n:
             num_refills += 1
     return num_refills
 
-
-x = [0, 50, 420, 712, 888, 888, 1112, 1500, 1900, 2300]
+x = [0, 400, 800, 1200, 1600, 2000, 2400, 2800, 3200] # dista
 n = len(x) - 2
 L = 400
 
