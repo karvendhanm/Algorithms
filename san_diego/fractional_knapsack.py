4fa8be0b6540dc46b3e8ed082ea9db440e5e67a2
Karvendhan M <karvsmech@gmail.com>
2020-01-24 17:05:53 +0530

    fractional knapsack completed

--- san_diego/fractional_knapsack.py
+++ san_diego/fractional_knapsack.py
@@ -3,31 +3,38 @@ from operator import itemgetter
 
 def unit_cost_func(l1, l2):
     lst = [0 for _ in range(len(l2))]
-    for idx, val in enumerate(l2):
+    for idx, val in enumerate(l1):
         if val != 0:
-            lst[idx]=l1[idx]/val
+            lst[idx] = l2[idx]/val
     return lst
 
-def knapsack(knapsack_capacity, items_quantity, items_cost):
+def knapsack(knapsack_capacity, items_quantity, items_total_cost):
     possible_max_val = 0
     num_items = len(items_quantity)
     each_item_Amount_taken = [0 for _ in range(len(items_quantity))]
 
-    lst = []
+    item_unit_price = unit_cost_func(items_quantity, items_total_cost)
 
-    for n, p, d, q in zip(range(num_items), items_quantity, items_cost, unit_cost_func(items_quantity, items_cost)):
-        lst.append((n, p, d, q))
+    for _ in range(num_items):
 
-    for idx in range(num_items):
         if knapsack_capacity == 0:
             return (possible_max_val, each_item_Amount_taken)
 
-        lst.sort(key=itemgetter(3), reverse=True)
+        idx = item_unit_price.index(max(item_unit_price))
+        q = min(items_quantity[idx], knapsack_capacity)
 
-    return lst
+        knapsack_capacity -= q
+        items_quantity[idx] -= q
+        each_item_Amount_taken[idx] = each_item_Amount_taken[idx]+q
+        possible_max_val = possible_max_val + (q * item_unit_price[idx])
+
+        if items_quantity[idx] == 0: item_unit_price[idx] = 0
+
+    return possible_max_val, each_item_Amount_taken
 
 
-knapsack(7, [4, 3, 2], [5, 4, 3])
+
+knapsack(22, [4, 8, 35], [60, 40, 1])
 
 
 
